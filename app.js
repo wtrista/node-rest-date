d5f8c6145d6b56ea5de917a898ff738279846455
Trista Wang <trista.x.wang@ibm.com>
2019-01-21 20:40:10 -0500

    added nodemon and morgan

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const dateRoutes = require('./api/routes/dates');
 const eventRoutes = require('./api/routes/events');
 
+app.use(morgan('dev'));
+
+// routes
 app.use('/dates', dateRoutes);
 app.use('/events', eventRoutes);
 
