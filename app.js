b23de3de4dc855b81b7104537f22c73eda5367c7
Trista Wang <trista.x.wang@ibm.com>
2019-01-21 20:48:15 -0500

    initial error handling

--- app.js
+++ app.js
@@ -11,4 +11,22 @@ app.use(morgan('dev'));
 app.use('/dates', dateRoutes);
 app.use('/events', eventRoutes);
 
+// if reach here, means no routes found
+app.use((req, res, next) => {
+    const error = new Error('No Fitting Rout Found...');
+    error.status = 404;
+    // forward the error request
+    next(error);
+});
+
+// handles all kinds of errors thrown from anywhere
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
+
 module.exports = app;
